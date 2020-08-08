7942e7452f3adb2c473eb39da39c4f8d3c79e284
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-08-08 13:37:22 +0200

    changes da inbox a lottery

--- compile.js
+++ compile.js
@@ -2,13 +2,13 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
 var input = {
     language: 'Solidity',
     sources: {
-        'Inbox.sol' : {
+        'Lottery.sol' : {
             content: source
         }
     },
